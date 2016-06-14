1965c2f0f3748e676f00d37c43a072a75371a1e8
rolandflutet <rolandflutet@users.noreply.github.com>
2016-06-14 14:35:29 +0100

    Update case.js

--- modules/case.js
+++ modules/case.js
@@ -25,6 +25,7 @@ function execute(req, res) {
 
     //DEBUG
     console.log("--------------res.connection.params-----------"+res.connection.params);
+    console.log(res.connection.params);
     // connection params response_url
 
 
